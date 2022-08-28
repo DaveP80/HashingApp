de583e63aa0d11b4c15022f1b95a3603a09542ea
DaveP80 <107325159+DaveP80@users.noreply.github.com>
2022-08-28 15:58:00 -0400

    Update AccountMDao.java

--- src/main/java/com/logic/dao/AccountMDao.java
+++ src/main/java/com/logic/dao/AccountMDao.java
@@ -2,13 +2,10 @@ package com.logic.dao;
 
 import com.logic.model.Account;
 import com.logic.util.MongoUtil;
-
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-
 import java.util.ArrayList;
 
 public class AccountMDao implements IMAccountDao{
@@ -16,9 +13,9 @@ public class AccountMDao implements IMAccountDao{
     MongoUtil mongoUtil = new MongoUtil();
     MongoDatabase database = mongoUtil.getDB();
     MongoCollection<Document> collection = database.getCollection("user");
+    
     @Override
     public void insertUser(Account a) throws InterruptedException {
-
 
         Document document = new Document("password", a.getPassword());
         mongoUtil.getUserCollection().insertOne(document);
@@ -31,6 +28,4 @@ public class AccountMDao implements IMAccountDao{
             System.out.println(users.get(0));
         }
     }
-
-
 }
